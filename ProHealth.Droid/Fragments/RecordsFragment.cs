d895c2faad764cacdde6bc928be223f8ef6100d9
SIVAMOHANREDDY <sivamohan.reddy@walkingtree.in>
2016-12-16 13:56:27 +0530

    Created Reports page with Recyclerview - Cherry Pick

--- ProHealth.Droid/Fragments/RecordsFragment.cs
+++ ProHealth.Droid/Fragments/RecordsFragment.cs
@@ -1,17 +1,13 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using Android.Support.V4.App;
 using Android.App;
-using Android.Content;
 using Android.OS;
 using Android.Runtime;
-using Android.Util;
 using Android.Views;
 using Android.Widget;
 using ProHealth.Core.ViewModels;
 using MvvmCross.Droid.Support.V7.Fragging.Attributes;
+using Android.Support.V7.Widget;
+using ProHealth.Droid.Adapters;
+using ProHealth.Droid.Helpers;
 
 namespace ProHealth.Droid.Fragments
 {
@@ -19,6 +15,49 @@ namespace ProHealth.Droid.Fragments
     [Register("prohealth.droid.fragments.RecordsFragment")]
     public class RecordsFragment : BaseFragment<RecordsViewModel>
     {
+        private RecyclerView recycler;
+        private RecyclerView.LayoutManager layoutMgr;
+        private Activity activity;
+        private BlockAdapter blockAdapter;
+
+
+        private CategoriesOfReports reportCategories;
+        private View view;
+
+        public CategoriesOfReports Reportcategories
+        {
+            get
+            {
+                if (reportCategories == null)
+                    reportCategories = new CategoriesOfReports();
+                return reportCategories;
+            }
+        }
+
         protected override int FragmentId => Resource.Layout.RecordsView;
+
+        public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+        {
+             view = base.OnCreateView(inflater, container, savedInstanceState);
+            recycler = initRecycler();
+            activity = this.Activity;
+            return view;
+        }
+        private RecyclerView initRecycler()
+        {
+            //Initialize the RecyclerView
+            recycler = view.FindViewById<RecyclerView>(Resource.Id.BlockRecycler);
+            layoutMgr = new GridLayoutManager(activity,2);
+            reportCategories = new CategoriesOfReports();
+            blockAdapter = new BlockAdapter(activity, reportCategories);
+            blockAdapter.ItemClick += OnItemClick;
+            recycler.SetLayoutManager(layoutMgr);
+            recycler.SetAdapter(blockAdapter);
+            return recycler;
+        }
+        public void OnItemClick(object sender, int e)
+        {
+            Toast.MakeText(activity, "item" + (e + 1), ToastLength.Short).Show();
+        }
     }
 }
